fc1ae5e8bd1e3346abf417cefba2cbec1902677d
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2026-02-28 17:14:27 -0600

    [HH-48] Build Header structure with placeholder links

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,3 +1,4 @@
+import { Header } from './components/Header';
 import { Hello } from './Hello';
 import { Info } from './Info';
 import { UsersList } from './UsersList';
@@ -5,6 +6,8 @@ import { UsersManager } from './UsersManager';
 
 export const App = () => (
   <div>
+    <Header />
+    
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />
--- imports/ui/components/Header.tsx
+++ imports/ui/components/Header.tsx
@@ -1,3 +1,41 @@
 export const Header = () => {
-  return <header>Hybrid Hiring Header</header>;
+  return (
+    <header className="header">
+      {/* Logo */}
+      <a href="/" className="header__logo">
+        Hybrid Hiring
+      </a>
+
+      {/* NavLink 
+        will switch to NavLink after routing is implemented 
+      */}
+      <nav className="header__nav">
+        <a href="/" className="header__link">
+          Home
+        </a>
+        <a href="/about" className="header__link">
+          About
+        </a>
+        <a href="/contact" className="header__link">
+          Contact
+        </a>
+        <a href="/jobs" className="header__link">
+          Jobs
+        </a>
+      </nav>
+
+      {/* Auth */}
+      <div className="header__auth">
+        <a href="/login" className="header__link">
+          Log In
+        </a>
+        <a href="/signup" className="header__link">
+          Sign Up
+        </a>
+      </div>
+
+      {/* Hamburger Button */}
+      <button className="header__menu">=</button>
+    </header>
+  );
 };
