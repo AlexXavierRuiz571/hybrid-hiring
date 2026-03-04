9074a1f33581853463ba770f1df6daca47efbf99
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2026-03-04 09:49:50 -0600

    [HH-48] Add auth state, admin link, and mobile menu toggle to Header

--- imports/ui/components/Header.tsx
+++ imports/ui/components/Header.tsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
-import { MobileNavOverlay } from "./MobileNavOverlay";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import { MobileNavOverlay } from '../layouts/MobileNavOverlay';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { isLoggedIn, isAdmin, logOut } = useAuth();
+
   return (
     <header className="site-header">
       {/* Logo */}
       <NavLink to="/" className="site-header__logo">
-        Hybrid Hiring
+        Hybrid Hiring Solutions
       </NavLink>
 
-      {/* NavLink 
-        will switch to NavLink after routing is implemented 
-      */}
+      {/* <Navigation> */}
       <nav className="site-header__nav">
         <NavLink to="/" className="site-header__link">
           Home
@@ -27,20 +28,45 @@ export const Header = () => {
         <NavLink to="/jobs" className="site-header__link">
           Jobs
         </NavLink>
+
+        {isAdmin && (
+          <NavLink to="/admin" className="site-header__link">
+            Admin
+          </NavLink>
+        )}
       </nav>
 
       {/* Auth */}
       <div className="site-header__auth">
-        <NavLink to="/login" className="site-header__link">
-          Log In
-        </NavLink>
-        <NavLink to="/signup" className="site-header__link">
-          Sign Up
-        </NavLink>
+        {isLoggedIn ? (
+          <button onClick={logOut}>Log Out</button>
+        ) : (
+          <>
+            <NavLink to="/login" className="site-header__link">
+              Log In
+            </NavLink>
+            <NavLink to="/signup" className="site-header__link">
+              Sign Up
+            </NavLink>
+          </>
+        )}
       </div>
 
       {/* Hamburger Button */}
-      <button className="site-header__hamburger">=</button>
+      <button className="site-header__hamburger" onClick={() => setIsMenuOpen(true)}>
+        <span className="site-header__hamburger-bar" />
+        <span className="site-header__hamburger-bar" />
+        <span className="site-header__hamburger-bar" />
+      </button>
+
+      {/* Mobile Nav Overlay */}
+      <MobileNavOverlay
+        isOpen={isMenuOpen}
+        onClose={() => setIsMenuOpen(false)}
+        isLoggedIn={isLoggedIn}
+        isAdmin={isAdmin}
+        onLogOut={logOut}
+      />
     </header>
   );
 };
